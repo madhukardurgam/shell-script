2035aeed3809bcdbbd6a9680e6897212484f01e3
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2024-12-30 23:20:12 +0530

    username and password wont appear on the cli

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "please enter your username::"
-read USERNAME #here user name is variable name
+read -s USERNAME 
+#here user name is variable name
+# -s for not showing the cli
 echo "username entered:: $USERNAME"
+
+echo "password entered:: $PASSWORD"
+
+read -s PASSWORD
