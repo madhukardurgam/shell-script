9b57a628ce4a71b6cc73b698f00bb1789e1930e6
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2024-12-30 23:03:45 +0530

    arguments passed

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 #space should not be there before and after the equal# $ represents the variable
+PERSON2=$2 #like x=1,y=2
+
+echo "$PERSON2::hi $PERSON1 bro"
+echo "$PERSON1::Hi $PERSON2 How are you"
+echo "$PERSON2::I am Good How are you $PERSON1 bro"
+echo "$PERSON1::after a long time we have meet each other"
