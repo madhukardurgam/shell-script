7812cf8b90061e3c49188ade4b5d0298ac7f2399
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2024-12-30 22:29:18 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "saketh::hi madhukar bro"
+echo "madhukar::Hi Saketh How are you"
+echo "saketh::I am Good How are you madhukar bro"
+echo "madhukar::after a long time we have meet each other"
