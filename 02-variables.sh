bc964e1d8ab3a2f5d79a8df5fe6ae6f03eca8bc4
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2024-12-30 22:56:33 +0530

    $ is for variable name

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$PERSON1=madhukar #space should not be there before and after the equal# $ represents the variable
-$PERSON2=saketh #like x=1,y=2
+PERSON1=madhukar #space should not be there before and after the equal# $ represents the variable
+PERSON2=saketh #like x=1,y=2
 
 echo "$PERSON2::hi $PERSON1 bro"
 echo "$PERSON1::Hi $PERSON2 How are you"
