ff609b462346661eddb2069a6709e5c70c0750db
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 20:21:58 +0530

    array of movies added

---
+++ 08-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#array contains the limited numbers of items
+MOVIES=("durgam" "madhukar" "dmk")
+
+echo "first movie: ${MOVIES[0]}"
+echo "first movie: ${MOVIES[1]}"
+echo "first movie: ${MOVIES[2]}"
