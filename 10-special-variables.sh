ae8724c189377ece3ce005b11464f3e63cd4bd12
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 20:39:49 +0530

    special variables

---
+++ 10-special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+$1 $2 $3
+echo "All variables passed: $@"
+echo "Number of variables passed: $#"
+echo "script name :$0"
+echo "present working directory :$PWD"
+echo "home directory of user: $HOME"
+echo "which user is running the script: $USER"
+echo "process id of current script: $$"
+sleep 30 & # & is for running the command on background
+echo "process id of previous ran command: $!" # "$!" is gives you the previous command of sleep
