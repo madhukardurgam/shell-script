9fab02240aedafe0a5be33bc433e0f3dcffbb0dc
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 22:42:35 +0530

    exit status

--- 14-install-sql-git.sh
+++ 14-install-sql-git.sh
@@ -5,8 +5,8 @@
 #if package is installed check wheather the user have the root access or not if not throw the error
 #if the user is haves root access then install the mysql
 # for the git also same procedure
-USERID=$( echo id -u)
-if [ $? -ne 0 ];
+USERID=$(id -u)
+if [ $USERID -ne 0 ];
 then
     echo "Need the root access to run the script command"
     exit 1
