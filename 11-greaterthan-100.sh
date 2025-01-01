54472c1047db545e60be464d71f10883c6872b42
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 21:23:36 +0530

    given number is greater or lesser than 100

---
+++ 11-greaterthan-100.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#given number is greater than or not
+#writing the if else condition to display the output
+
+NUMBER=$1
+if [$NUMBER -gt 100] 
+then
+ echo "Given number is greater than 100: $NUMBER"
+else
+ echo "Given number is lesser than 100: $NUMBER"
+fi
