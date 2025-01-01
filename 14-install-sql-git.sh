c2a3a42222064e0c758c18c2bb2c6f08227f50d7
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 22:28:26 +0530

    exit status

--- 14-install-sql-git.sh
+++ 14-install-sql-git.sh
@@ -7,14 +7,14 @@
 # for the git also same procedure
 
 dnf list installed mysql
-if [ $? -ne 0];
+if [ $? -ne 0 ];
 then
     echo "Package is not installed"
 else
     echo "Package is already installed"
 fi
 USERID=$(id -u)
-if [ $? -ne 0];
+if [ $? -ne 0 ];
 then
     echo "Need the root access to run the script command"
     exit 1
@@ -22,7 +22,7 @@ else
     echo "You have the access to run the script"
 fi
 dnf install mysql -y
-if [ $? -ne 0];
+if [ $? -ne 0 ];
 then
     echo "Package is not installed"
 else
