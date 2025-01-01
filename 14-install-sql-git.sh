050a023fe595e79e3bfec4aa70513c7e915865f5
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 22:32:20 +0530

    exit status

--- 14-install-sql-git.sh
+++ 14-install-sql-git.sh
@@ -5,21 +5,21 @@
 #if package is installed check wheather the user have the root access or not if not throw the error
 #if the user is haves root access then install the mysql
 # for the git also same procedure
-
-dnf list installed mysql
-if [ $? -ne 0 ];
-then
-    echo "Package is not installed"
-else
-    echo "Package is already installed"
-fi
 USERID=$(id -u)
+dnf list installed mysql
 if [ $? -ne 0 ];
 then
     echo "Need the root access to run the script command"
     exit 1
 else
     echo "You have the access to run the script"
+fi
+dnf list installed mysql
+if [ $? -ne 0 ];
+then
+    echo "Package is not installed"
+else
+    echo "Package is already installed"
 fi
 dnf install mysql -y
 if [ $? -ne 0 ];
