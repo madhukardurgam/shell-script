80b8ceeff85aa40be54f0243db816263c2ea6a5e
madhukardurgam <madhukar.durgam.8.7@gmail.com>
2025-01-01 22:00:25 +0530

    exit status

--- 13-root-user.sh
+++ 13-root-user.sh
@@ -4,7 +4,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ];
 then
-    echo "you are root user"
+    echo "you are not root user"
 else
-    echo "You are not the root user to run the script"
+    echo "You are the root user to run the script"
 fi
